4307b4f6a0afae426da8edfaba7d5cbc11f49158
practac <107167876+practac@users.noreply.github.com>
2024-10-11 13:19:55 +0900

    [week2] merge main

--- app/src/main/java/com/example/umc_android_study/MainActivity.kt
+++ app/src/main/java/com/example/umc_android_study/MainActivity.kt
@@ -1,16 +1,46 @@
 package com.example.umc_android_study
 
+import com.example.umc_android_study.R
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // activity_main.xml 파일을 설정
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+
+        // 첫 화면에 보여질 Fragment 설정
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
+
+        bottomNavigationView.setOnItemSelectedListener { item: MenuItem ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.navigation_home -> selectedFragment = HomeFragment()
+                R.id.navigation_stamp -> selectedFragment = StampFragment()
+                R.id.navigation_mypage -> selectedFragment = MyPageFragment()
+            }
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit()
+            }
+            true
+        }
 
         // Edge-to-Edge 설정
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
