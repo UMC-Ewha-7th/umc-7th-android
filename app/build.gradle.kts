3e1affc96f851986ba7a17817095f9c9b65543ba
OSSU <90602694+SuHyeon00@users.noreply.github.com>
2024-10-11 15:57:26 +0900

    [Week2] Activity와 Fragment_메이 (#6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
