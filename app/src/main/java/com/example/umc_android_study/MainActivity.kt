511a7869b549aff27c8f009c1e3cc786848eecbf
practac <107167876+practac@users.noreply.github.com>
2024-10-11 18:08:58 +0900

    [week2] merge main

--- app/src/main/java/com/example/umc_android_study/MainActivity.kt
+++ app/src/main/java/com/example/umc_android_study/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.example.umc_android_study
 
-import com.example.umc_android_study.R
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -42,63 +37,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        // Edge-to-Edge 설정
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        // 각 ImageView를 찾아서 클릭 이벤트 설정
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        val imageView2 = findViewById<ImageView>(R.id.imageView2)
-        val imageView3 = findViewById<ImageView>(R.id.imageView3)
-        val imageView4 = findViewById<ImageView>(R.id.imageView4)
-        val imageView5 = findViewById<ImageView>(R.id.imageView5)
-
-        // ImageView 1 클릭 이벤트
-        imageView.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                nextpage::class.java // 다음 페이지로 이동하는 액티비티 설정
-            )
-            startActivity(intent)
-        }
-
-        // ImageView 2 클릭 이벤트
-        imageView2.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                nextpage::class.java
-            )
-            startActivity(intent)
-        }
-
-        // ImageView 3 클릭 이벤트
-        imageView3.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                nextpage::class.java
-            )
-            startActivity(intent)
-        }
-
-        // ImageView 4 클릭 이벤트
-        imageView4.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                nextpage::class.java
-            )
-            startActivity(intent)
-        }
-
-        // ImageView 5 클릭 이벤트
-        imageView5.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                nextpage::class.java
-            )
-            startActivity(intent)
-        }
     }
 }
