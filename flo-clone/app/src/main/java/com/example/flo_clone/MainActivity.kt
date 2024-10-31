a1a0a02dbca0e50e9c0cb993195b2a939985adea
OSSU <o.ss.u2056@gmail.com>
2024-10-31 20:26:32 +0900

    :sparkles: 홈 화면 panel 레이아웃 구현, 액션 바 및 상태바 투명 설정 추가

--- flo-clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
+++ flo-clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
@@ -5,7 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.flo_clone.databinding.ActivityMainBinding
 
@@ -23,14 +22,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_look, R.id.navigation_search, R.id.navigation_locker
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
