3ed6408da703286deec2ce025c7d61e0e5d88edf
OSSU <o.ss.u2056@gmail.com>
2024-10-31 20:27:55 +0900

    :sparkles: 홈 화면 panel 레이아웃 구현, 액션 바 및 상태바 투명 설정 추가

--- flo-clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
+++ flo-clone/app/src/main/java/com/example/flo_clone/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.flo_clone.databinding.ActivityMainBinding
 
