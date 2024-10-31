bad8040f35ce7008c4cec522e985bc625e004b4b
OSSU <o.ss.u2056@gmail.com>
2024-11-01 00:55:28 +0900

    :sparkles: 홈 화면 레이아웃 구성

--- flo-clone/app/src/main/java/com/example/flo_clone/ui/look/LookViewModel.kt
+++ flo-clone/app/src/main/java/com/example/flo_clone/ui/look/LookViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class LookViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is look Fragment"
     }
     val text: LiveData<String> = _text
 }
